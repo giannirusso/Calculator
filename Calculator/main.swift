1139d9d0896aad6467e2f8b4902784631d066933
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2021-05-02 17:35:07 +0300

    created functions

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -2,10 +2,36 @@
 //  main.swift
 //  Calculator
 //
-//  Created by D K on 02.05.2021.
+//  Created by D K on 30.04.2021.
 //
 
 import Foundation
 
-print("Hello, World!")
+struct Number {
+    var variable1 = Int()
+    var variable2 = Int()
+    
+    func addition(){
+        let sum = variable1 + variable2
+    }
+    
+    func subtraction() {
+        let subt = variable1 - variable2
+    }
+    
+    func multiplication() {
+        let multiply = variable1 * variable2
+    }
+    
+    func division() {
+        let div = variable1 / variable2
+    }
+    
+    func power() {
+        let powerOperation = variable1 ^ variable2
+    }
+
+} 
+
+
 
