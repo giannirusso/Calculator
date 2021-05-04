03036936086f8c686bae05869f3459a0b7c28c3e
Djanibek Khudaybergenov <djanibek.k@gmail.com>
2021-05-04 23:27:06 +0300

    Added operations

--- Calculator/main.swift
+++ Calculator/main.swift
@@ -11,27 +11,80 @@ struct Number {
     var variable1 = Int()
     var variable2 = Int()
     
-    func addition(){
-        let sum = variable1 + variable2
-    }
-    
-    func subtraction() {
-        let subt = variable1 - variable2
-    }
-    
-    func multiplication() {
-        let multiply = variable1 * variable2
-    }
-    
-    func division() {
-        let div = variable1 / variable2
-    }
-    
-    func power() {
-        let powerOperation = variable1 ^ variable2
+
+    func addition() -> Int {
+        variable1 + variable2
+        
     }
 
-} 
+    func subtraction() -> Int {
+        variable1 - variable2
+        
+    }
+
+    func multiplication() -> Int {
+        variable1 * variable2
+     
+    }
+
+    func division() -> Int {
+        variable1 / variable2
+        
+    }
+
+    func power() -> Decimal {
+        pow(Decimal(variable1), (variable2))
+        
+    }
+}
+
+//let result = Number.init(variable1: 2, variable2: 4)
+
+
+let welcomeLine = "Это калькулятор. Введите Х "
+print(welcomeLine)
+
+let xNumber: Int = Int(readLine()!)!
+print(xNumber, "Вы ввели число \(xNumber)")
+
+let welcomeLineTwo = "Теперь введите Y "
+print(welcomeLineTwo)
+
+let yNumber: Int = Int(readLine()!)!
+print(yNumber, "Вы ввели число \(yNumber)")
+
+let desiredOperation = "Выберите желаемое действие с числами из списка"
+print(desiredOperation)
+
+//let powerResult: Decimal = Number.init(variable1: xNumber, variable2: yNumber).power()
+//print("Результат возведения в степень \(powerResult)")
+
+let arrayOfOperations: [Int] = [1, 2, 3, 4, 5]
+let operationNumber = Int(readLine()!)!
+print(operationNumber)
+
+switch operationNumber {
+case 1: let number = Number.init(variable1: xNumber, variable2: yNumber)
+    print("Результат операции сложения \(number.addition())")
+case 2: let number = Number.init(variable1: xNumber, variable2: yNumber)
+    print("Результат операции вычитания \(number.subtraction())")
+case 3: let number = Number.init(variable1: xNumber, variable2: yNumber)
+    print("Результат операции умножения \(number.multiplication())")
+case 4: let number = Number.init(variable1: xNumber, variable2: yNumber)
+    print("Результат операции деления \(number.division())")
+case 5: let number = Number.init(variable1: xNumber, variable2: yNumber)
+    print("Результат операции степени \(number.power())")
+default:
+    print("что вы хотите?")
+}
+
+
+
+
+
+
+
+
 
 
 
